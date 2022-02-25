23207fbd007321d04db19a2803c2f9db6d00871f
Paul <pavel.m.pashax12@gmail.com>
2022-02-26 00:14:07 +0300

    commit 26.02.2022

--- src/component/EncounterGenerator/index.js
+++ src/component/EncounterGenerator/index.js
@@ -1,10 +1,100 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './style.css'
+import axios from "axios";
+import resources from "../../resource.json";
+import {Scrollbars} from "react-custom-scrollbars-2";
+import Block from "./Block/Block";
+import {nanoid} from "nanoid";
+import Filter from "./Filters";
 
 export default function EncounterGenerator() {
-    return(
-        <div>
-            Encounter Page
-        </div>
-    )
+    const [monsters, setMonster] = useState([{}]);
+
+    const [formData, setFormData] = React.useState({
+        fightParty: [], playerLevel: '', playerClass: '', fightChallenge: '', isMixedType: false
+    })
+    const [loadData, setLoadData] = React.useState({ playersLevel: [], difficulty: '', mixedTypes: false})
+
+    function handleAdd(event) {
+        event.preventDefault();
+
+        setFormData(prevState => {
+            return {
+                ...prevState,
+                fightParty: [...prevState.fightParty, prevState.playerLevel + ' level ' + prevState.playerClass]
+            }
+        })
+        handleChange(event)
+    }
+
+    function handleDel(event) {
+        event.preventDefault();
+        setFormData(prevState => {
+            return {
+                ...prevState,
+                fightParty: prevState.fightParty.filter((element, index) => index < prevState.fightParty.length - 1)
+            }
+        })
+        console.log(formData.fightParty)
+        handleChange(event);
+    }
+
+    console.log(formData);
+
+
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData, [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+
+    function generateEncounter(event) {
+        event.preventDefault();
+
+        setLoadData(() => {
+            return{
+                playersLevel: formData.fightParty,
+                difficulty: formData.fightChallenge,
+                mixedTypes: formData.isMixedType
+            }
+        });
+    }
+
+    useEffect(() => {
+        console.log(loadData);
+        axios.post(resources.createEncounter, loadData)
+            .then(value => {
+                console.log(value);
+                return setMonster(value.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, [loadData, setMonster]);
+
+    return (<main className='library--container'>
+            <div className='library--boxes'>
+                <div className='library--box-1'><h1>Create Encounter</h1>
+                    {(monsters.length > 1 && <Scrollbars
+                        autoHide
+                        universal
+                        autoHeight
+                        autoHeightMin={500}>
+                        <div className='library--contest'>
+                            {monsters.map(value => <Block key={nanoid()} value={value}/>)}
+                        </div>
+                    </Scrollbars>)}</div>
+
+                <div className='library--box-2'>
+                    <h3>Encounter filters</h3>
+                    <Filter value={{
+                        data: formData, change: handleChange, add: handleAdd, del: handleDel, gener: generateEncounter
+                    }}/>
+                </div>
+            </div>
+
+        </main>)
 }
--- src/component/MonsterLibrary/index.js
+++ src/component/MonsterLibrary/index.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import resources from '../../resource.json';
 import './style.css';
 import Block from "./Block/Block";
 import {nanoid} from "nanoid";
+import Filter from "./Filters";
+import {Scrollbars} from "react-custom-scrollbars-2";
 
 
 export default function MonsterLibrary(){
+
+    const [monsters, setMonster]  = useState([{}]);
 
     const [formData, setFormData] = React.useState(
         {
@@ -13,15 +19,13 @@ export default function MonsterLibrary(){
             minMonsterChallenge:'10',
             maxMonsterChallenge:'155000',
             monsterOutlook:'',
-            source:true,
-            legendaryAction:false,
-            specialSkills:false
+            isSource:false,
+            isLegendaryAction:false,
+            isSpecialSkills:false
         }
     )
-    console.log(formData.monsterType)
 
     function handleChange(event) {
-        console.log(event)
         const {name, value, type, checked} = event.target
         setFormData(prevFormData => {
             return {
@@ -31,64 +35,35 @@ export default function MonsterLibrary(){
         })
     }
 
+    useEffect(() => {
+       axios.post(resources.postCriteria, formData)
+            .then(value => {
+                return setMonster(value.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },[formData, setMonster]);
 
 
-    // const testclass ={name:'Acolyte', challenge:'1000', meta:'Some meta'};
     return(
     <main className='library--container'>
         <div className='library--boxes'>
 
-            <div className='library--box-1'>
-                <h1>Our monster</h1>
-                 {/*{(monster.length>1 && monster.map(value =><Block key={nanoid()} value={value} /> ))}*/}
-            </div>
+                <Scrollbars
+                    autoHide
+                    className='library--box-1'
+                    universal
+                    autoHeight
+                    autoHeightMax={600}>
+                    <div className='library--contest'>
+                        <h1>Our monster</h1>
+                        {(monsters.length>1 && monsters.map(value =><Block key={nanoid()} value={value}/>))}
+                    </div>
+                </Scrollbars>
             <div className='library--box-2'>
                 <h3>Filters</h3>
-                <form className='filter--container'>
-                    <p className='filter--text-head'>Monster challenge</p>
-                    <label className='filter--text'>Monster min challenge</label>
-                    <select
-                        id="minMonsterChallenge"
-                        value={formData.minMonsterChallenge}
-                        onChange={handleChange}
-                        name="minMonsterChallenge"
-                    >
-                        <option value="10">0</option>
-                        <option value="25">1/8</option>
-                        <option value="50">1/4</option>
-                        <option value="100">1/2</option>
-                        <option value="200">1</option>
-                        <option value="450">2</option>
-                        <option value="700">3</option>
-                        <option value="1100">4</option>
-                        <option value="1800">5</option>
-                        <option value="2300">6</option>
-                        <option value="2900">7</option>
-                        <option value="3900">8</option>
-                        <option value="5000">9</option>
-                        <option value="5900">10</option>
-                        <option value="7200">11</option>
-                        <option value="8400">12</option>
-                        <option value="10000">13</option>
-                        <option value="11500">14</option>
-                        <option value="13000">15</option>
-                        <option value="15000">16</option>
-                        <option value="18000">17</option>
-                        <option value="20000">18</option>
-                        <option value="22000">19</option>
-                        <option value="25000">20</option>
-                        <option value="33000">21</option>
-                        <option value="41000">22</option>
-                        <option value="50000">23</option>
-                        <option value="62000">24</option>
-                        <option value="75000">25</option>
-                        <option value="90000">26</option>
-                        <option value="105000">27</option>
-                        <option value="120000">28</option>
-                        <option value="135000">29</option>
-                        <option value="155000">30</option>
-                    </select>
-                </form>
+                <Filter value={{data:formData, change:handleChange}} />
             </div>
         </div>
 
